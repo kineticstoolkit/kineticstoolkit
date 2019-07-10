b197d743d2810e3545ad8fb747b7e422de1e990d
Félix Chénier <felix@felixchenier.com>
2019-07-10 10:39:58 -0400

    First commit on feature/TimeSeries

--- timeseries.py
+++ timeseries.py
@@ -1,4 +1,6 @@
 """
+Module that manages the TimeSeries class.
+
 TimeSeries
 ==========
 
@@ -13,48 +15,106 @@ Created on Thu Jun  6 11:07:32 2019
 import matplotlib.pyplot as plt
 import numpy as np
 import ktkcore
+from copy import deepcopy
 
-class TimeSeries(ktkcore.ObjDict):
+
+class TimeSeries():
     """
-    This class implements a timeseries in a close way to the Matlab's
-    timeseries object.
+A class that implements timeseries.
 
-    Parameters
+    This class implements a timeseries in a way that resembles the timeseries
+    and tscollection found in Matlab.
+
+    Attributes
     ----------
-    name : String, optional
-        Name of the timeseries. The default is ''.
-    data : TYPE, optional
-        DESCRIPTION. The default is [].
-    time : TYPE, optional
-        DESCRIPTION. The default is [].
-    dataunit : TYPE, optional
-        DESCRIPTION. The default is ''.
-    timeunit : TYPE, optional
-        DESCRIPTION. The default is 's'.
-    
-    Returns
-    -------
-    None.
-    
+    time : 1d np.array
+        A time vector common to all data of the timeseries.
+    data : dict
+        Dictionary of np.arrays containing the timeseries data.
+    time_unit : str
+        The time unit. The default is 's'.
+    data_unit : dict
+        Dictionary of strings containing the data units.
+    events : list of ktk.Event
+        A list of events.
     """
 
     def __init__(
-            self, name='', data=[], time=[], dataunit='', timeunit='s'):
-        if len(name) > 0: self[name] = data
-        self.Time = time
-        self.Info = ktkcore.ObjDict()
-        self.Info.TimeUnit = timeunit
-    
+            self, time=np.array([]), data=dict(), time_unit='s',
+            data_unit=dict(), events=[]):
+        self.time = np.array(time)
+        self.data = data
+        self.time_unit = time_unit
+        self.data_unit = data_unit
+        self.events = events
+
+    def __str__(self):
+        """
+        Return the string representation of the timeseries.
+
+        Returns
+        -------
+        str: The string representation of the timeseries.
+
+        """
+        str_out = 'TimeSeries'
+        str_out += '\n  time: array of shape ' + str(np.shape(self.time))
+        str_out += '\n  time_unit: ' + str(self.time_unit)
+        str_out += '\n  data:'
+        for key in self.data.keys():
+            str_out += ('\n    ' + str(key) + ': array of shape '
+                        + str(np.shape(self.data[key])))
+
+        str_out += '\n  data_unit: '
+        for key in self.data_unit.keys():
+            str_out += ('\n    ' + str(key) + ': ' + str(self.data_unit[key]))
+
+        str_out += '\n  events: ' + str(len(self.events))
+
+        return(str_out)
+
+    def __repr__(self):
+        """
+        Return the string representation of the timeseries.
+
+        This method simply calls __str__(self).
+
+        Returns
+        -------
+        str: The string representation of the timeseries.
+
+        """
+        return(str(self))
+
+    def copy(self):
+        """
+        Return a deep copy of the timeseries.
+
+        Usage
+        -----
+        >>> ts1 = ktk.TimeSeries(...)
+        >>> ts2 = ts1.copy()
+
+        Returns
+        -------
+        TimeSeries: An identical, deep copy of the timeseries where each
+        original element has been copied so that no reference exists between
+        the original and returned timeseries.
+        """
+        # TODO Unit test
+        return deepcopy(self)
+
+
     def save(self, file_name): #TODO, still not what I want.
         np.save(file_name, self, allow_pickle=True)
-    
+
     def load(file_name): #TODO, still not what I want.
         temp = np.load(file_name, allow_pickle=True)
         temp = temp.tolist()
         return(temp)
-        
+
     def plot(self):
-        
+
         #How many plots
         thekeys = self.keys()
         nplots = 0
@@ -65,21 +125,21 @@ class TimeSeries(ktkcore.ObjDict):
         #Now plot
         iplot = 1
         for thekey in thekeys:
-            
+
             if thekey != 'Info' and thekey != 'Time' and thekey[0] != '_':
-                
+
                 if iplot == 1:
                     ax = plt.subplot(nplots,1,iplot)
                 else:
                     plt.subplot(nplots,1,iplot, sharex=ax)
-                    
+
                 plt.plot(self.Time, self[thekey], linewidth=1)
-                
+
                 if (thekey + 'Unit') in self.Info:
                     plt.ylabel(thekey + ' (' + self.Info[thekey + 'Unit'] + ')')
                 else:
                     plt.ylabel(thekey)
-                
+
                 iplot = iplot+1
 
         plt.xlabel('Time (' + self.Info['TimeUnit'] + ')')
