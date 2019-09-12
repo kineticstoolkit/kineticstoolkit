d65a50c502e276ff90b7f40df856722d111151ea
Félix Chénier <felix@felixchenier.com>
2019-09-11 20:56:02 -0400

    Changed the unit testing to pytest.

--- ktk/dev/__init__.py
+++ ktk/dev/__init__.py
@@ -10,33 +10,12 @@ Date: July 2019
 import ktk
 import subprocess as _subprocess
 import webbrowser as _webbrowser
-import inspect as _inspect
-
-from . import test_timeseries
-from . import test_loadsave
-from . import test_filters
-from . import test_pushrimkinetics
+import pytest
 
 
 def run_tests(module=None):
     """Run all unit tests."""
-
-    # If called without arguments, call again with all test modules
-    if module is None:
-        run_tests(test_timeseries)
-        run_tests(test_loadsave)
-        run_tests(test_filters)
-        run_tests(test_pushrimkinetics)
-
-    # If called with test module, run all functions in this module
-    else:
-        print('----------------------------------------------')
-        print(f'Running tests from {module}')
-        tests = _inspect.getmembers(module, _inspect.isfunction)
-        for test in tests:
-            if test[0][0] != '_':  # Not a private function
-                print(test[0])  # Function name
-                test[1]()   # Execute function
+    pytest.main([ktk.config['RootFolder']])
 
 
 def generate_tutorials():
