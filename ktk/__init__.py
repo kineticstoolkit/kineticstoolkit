dd1f2f21f0757e7c478722faa0c95edd54065690
Félix Chénier <felix@felixchenier.com>
2020-05-01 17:45:00 -0400

    Stripped ktk/__init__.py so that the inexistent modules are not loaded.

--- ktk/__init__.py
+++ ktk/__init__.py
@@ -37,44 +37,4 @@ config['IsLinux'] = True if _platform.system() == 'Linux' else False
 # ---------------------------
 
 from ktk._timeseries import TimeSeries, TimeSeriesEvent
-from ktk._dbinterface import DBInterface
-from ktk._player import Player
-from ktk._loadsave import load, loadmat, save
-from ktk import filters
-from ktk import gui
-from ktk import mplhelper
-from ktk._tools import explore, terminal, update, tutorials
-from ktk import geometry
-from ktk import kinematics
-from ktk import pushrimkinetics
-from ktk import inversedynamics
-from ktk import dev
-from ktk import cycles
-from ktk import _repr
-
-# ---------------------------
-# Customizations
-# ---------------------------
-
-# Modify the repr function for dicts in IPython
-try:
-    import IPython as _IPython
-    _ip = _IPython.get_ipython()
-    formatter = _ip.display_formatter.formatters['text/plain']
-    formatter.for_type(dict, lambda n, p, cycle:
-                       _repr._ktk_format_dict(n, p, cycle))
-except Exception:
-    pass
-
-# Set alternative defaults to matplotlib
-_mpl.rcParams['figure.figsize'] = [10, 5]
-_mpl.rcParams['figure.dpi'] = 75
-_mpl.rcParams['lines.linewidth'] = 1
-
-# Set a custom color order that is compatible with 'char' colors, and that
-# begins with RGB so that it is compatible with most XYZ color orders in other
-# visualization softwares.
-gui.set_color_order('xyz')
-
-# Select default mode for numpy
-_np.set_printoptions(suppress=True)
+from ktk._tools import explore, terminal
