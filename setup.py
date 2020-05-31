61550d1d360207e4ac18dc32ec38a2d29b4ed469
Félix Chénier <felix@felixchenier.com>
2020-05-31 15:02:04 -0400

    0.1.3 released.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='ktk',
-    version='0.1.2',
+    version='0.1.3',
     description='A pure-python library to facilitate research in biomechanics.',
     long_description=long_description,
     long_description_content_type="text/markdown",
