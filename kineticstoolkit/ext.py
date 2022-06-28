d04bc1c51d597771f8a93674e84dbe9603b9d80c
Félix Chénier <chenier.felix@uqam.ca>
2022-06-28 16:38:37 -0400

    [master] #95 Added documentation for loading non-installed extensions. Unit tests to do.

--- kineticstoolkit/ext.py
+++ kineticstoolkit/ext.py
@@ -28,8 +28,8 @@ __license__ = "Apache 2.0"
 import warnings
 import importlib
 import pkgutil
-import sys
-from typing import List
+import os
+from typing import List, Optional, Set, Sequence
 
 
 # def __getattr__(module_name):
@@ -47,34 +47,33 @@ from typing import List
 
 def _import_extensions() -> List[str]:
     """
-    Import all extensions found on PYTHONPATH.
+    Import all installed kineticstoolkit extensions.
 
     Any module that begins by 'kineticstoolkit_' and that is on PYTHONPATH will
-    be found and imported as an extension in the kineticstoolkit.ext namespace.
-
-    Parameters
-    ----------
-    None.
+    be imported in the kineticstoolkit.ext namespace.
 
     Returns
     -------
     A list of the imported extension names.
 
+    To test an extension in development that is not yet installed, start by
+    adding the extension's folder to the PYTHONPATH. This folder is the outer
+    one: it contains the folder than contains the __init__.py file. For
+    example, for an extension named kineticstoolkit_awesomestuff located in
+    the folder extension_folder:
+
+        import kineticstoolkit as ktk
+        import sys
+        sys.path.append('extension_folder')
+        ktk.import_extensions()
+
     """
-    # Dynamically import extensions
-
-    # Clear loaded_extensions and start over
-    del imported_extensions[:]
-
-    # Scan PYTHONPATH and current directory
-    the_path = sys.path + ["."]
-
-    for finder, name, ispkg in pkgutil.iter_modules(the_path):
+    for finder, name, ispkg in pkgutil.iter_modules():
         if name.startswith("kineticstoolkit_"):
             short_name = name[len("kineticstoolkit_") :]
             try:
                 globals()[short_name] = importlib.import_module(name)
-                imported_extensions.append(short_name)
+                imported_extensions.add(short_name)
             except Exception:
                 warnings.warn(
                     f"There have been an error importing the {name} "
@@ -82,11 +81,17 @@ def _import_extensions() -> List[str]:
                     f"more insights on the cause of the error."
                 )
 
-    return imported_extensions
+    return list(imported_extensions)
 
 
-imported_extensions = []  # type: List[str]
+imported_extensions = set()  # type: Set[str]
 
 
 def __dir__():
     return imported_extensions
+
+
+if __name__ == "__main__":  # pragma: no cover
+    import doctest
+
+    doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE)
