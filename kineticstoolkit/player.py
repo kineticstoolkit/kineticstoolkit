29363a039b5b0df453d50ef67c130273326fecd4
Félix Chénier <chenier.felix@uqam.ca>
2024-02-27 14:05:11 -0500

    Revert "Changed back _to_animation() to html5 video. It is much cooler in the resulting page, with play/stop controls, full screen, etc. and it doesn't generate a temp file."
    This reverts commit 1b288bd3af375055f63595e9e3f8ed6790e6e224.

--- kineticstoolkit/player.py
+++ kineticstoolkit/player.py
@@ -1613,34 +1613,18 @@ class Player:
         A FuncAnimation to be displayed by Jupyter notebook.
 
         """
-        mpl.rcParams["animation.html"] = "html5"
+        try:
+            from IPython.display import Video
+        except ModuleNotFoundError:
+            raise RuntimeError(
+                "This function must be run in an IPython session."
+            )
 
-        self.playback_speed = 0
         self._mpl_objects["Figure"].set_size_inches(6, 4.5)  # Half size
         self._mpl_objects["Figure"].tight_layout()
-        self.pause()
-        self.index = 0
-
-        # We create a specific animation and callback, since all processing
-        # will be done offline. We set a very long delay between frames but
-        # this is just so that the animation didn't advance by itself by the
-        # time recording has started.
-        def advance(args):
-            self.title = (
-                f"{self.current_index}/{len(self._contents.time)}: "
-                f"{self.current_time:.3f} s."
-            )
-            self.current_index = args
-
-        anim = animation.FuncAnimation(
-            self._mpl_objects["Figure"],
-            advance,  # type: ignore
-            frames=len(self._contents.time),
-            interval=33,
-        )  # 30 ips
-
+        self.to_mp4("temp.mp4", show_progress_bar=False)
         plt.close(self._mpl_objects["Figure"])
-        return anim
+        return Video("temp.mp4", embed=True, html_attributes="loop autoplay")
 
     # %% Public methods
     def play(self) -> None:
@@ -1766,11 +1750,12 @@ class Player:
         # this is just so that the animation didn't advance by itself by the
         # time recording has started.
         def advance(args):
+            self.current_index = args
             self.title = (
                 f"{self.current_index}/{len(self._contents.time)}: "
                 f"{self.current_time:.3f} s."
             )
-            self.current_index = args
+            self._fast_refresh()
 
         def progress_callback(current_frame: int, total_frames: int):
             print(f"Saving frame {current_frame}/{total_frames}")
